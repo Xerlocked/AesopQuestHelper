f85a251059335c5d8c17cabd232d6fa166899c97
Xerlocked <sherlock_203b@naver.com>
2022-07-20 19:08:31 +0900

    v1
    - Update JsonHelper class
    - Update tk ui

--- Src/main.py
+++ Src/main.py
@@ -1,21 +1,39 @@
 import tkinter
 import tkinter.font
+import tkinter.ttk
 
 MainWindow = tkinter.Tk()
 
+# Setting
 MainWindow.title("Aesop Quest Helper") # title
 MainWindow.geometry("480x720+100+100") # size
 MainWindow.resizable(False, False) # resize option
-
 DefaultFont = tkinter.font.Font(family="맑은 고딕")
+
+EQuestType = ["일반", "카운트"]
 
 # Label
 lQuestName = tkinter.Label(MainWindow, text="퀘스트 이름", width=45, height=20, fg="red", font=DefaultFont)
 lQuestType = tkinter.Label(MainWindow, text="퀘스트 종류", width=45, height=20, fg="red", font=DefaultFont)
 lQuestGoal = tkinter.Label(MainWindow, text="퀘스트 목표", width=45, height=20, fg="red", font=DefaultFont)
 
-lQuestName.pack()
-lQuestType.pack()
-lQuestGoal.pack()
+# Button
+bQuestAppend = tkinter.Button(MainWindow, text="추가", width=15, height=20, font=DefaultFont)
+bQuestRemoveAt = tkinter.Button(MainWindow, text="삭제", width=15, height=20, font=DefaultFont)
+bQuestClear = tkinter.Button(MainWindow, text="전체삭제", width=15, height=20, font=DefaultFont)
+
+# TextField
+tQuestName = tkinter.Entry(MainWindow)
+tQuestGoal = tkinter.Entry(MainWindow)
+
+# ComboBox
+cQuestType = tkinter.ttk.Combobox(MainWindow, height="10", values=EQuestType, state="readonly")
+cQuestType.set("선택")
+
+#lQuestName.pack()
+#lQuestType.pack()
+#lQuestGoal.pack()
+bQuestAppend.pack()
+cQuestType.pack()
 
 MainWindow.mainloop()
--- Src/sub.py
+++ Src/sub.py
@@ -1,17 +1,40 @@
 import json
-from matplotlib.font_manager import json_load
-from sqlalchemy import null
+
 
 class JsonHelper:
-    
+
     def __init__(self):
         self.quest_list = []
-        
-    def AppendQuest(self, qname, qtype, qgoal):
-        if qname != null and  qtype != null and qgoal != null :
-            newQuest = {"name": qname, "type": qtype, "goal": qgoal}
-            self.quest_list.append(newQuest)
 
+    def AppendQuest(self, qname, qtype, qgoal):
+        if qname is not None and qtype is not None and qgoal is not None:
+            new_quest = {"name": qname, "type": qtype, "goal": qgoal}
+            self.quest_list.append(new_quest)
+
+    def ClearQuest(self):
+        self.quest_list.clear()
+
+    def RemoveAtQuest(self, index):
+        self.quest_list.pop(index)
+
+    def WriteJson(self):
+        if len(self.quest_list) > 0:
+            with open('Quest.json', 'w') as json_file:
+                json.dump(self.quest_list, json_file, indent=4)
+
+    def __ReadJson(self, path):
+        with open(path, 'r') as f:
+            data = json.load(f)
+        return data
+
+    def AppendJsonData(self, path):
+        json_data = self.__ReadJson(path)
+        if json_data is not None:
+            for data in json_data:
+                self.AppendQuest(data["name"], data["type"], data["goal"])
+
+    def Refresh(self):
+        return self.quest_list
 
 listQuest = []
 dictQuest1 = {"name": "save princess", "type": "count", "goal": 2}
@@ -28,9 +51,9 @@ json_load = json.loads(json_string)
 
 print(json_load[0]["name"])
 
-#json_str = json.dumps(json_obj, indent=4)
-#print(json_str)
+# json_str = json.dumps(json_obj, indent=4)
+# print(json_str)
 
-#json_load = json.loads(json_str)
+# json_load = json.loads(json_str)
 
-#print(json_load['quest'][1]['name'])
+# print(json_load['quest'][1]['name'])
